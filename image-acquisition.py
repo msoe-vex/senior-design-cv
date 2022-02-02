9d4fd71132071649178b702c3293377373bba0e8
TrentonBowser <trenton.bowser25@gmail.com>
2022-02-02 00:12:42 -0600

    Updated image collection for Realsense camera

--- image-acquisition.py
+++ image-acquisition.py
@@ -1,10 +1,9 @@
+import pyrealsense2 as rs
 import cv2
-import os
+import numpy as np
 
-from os import path
 from time import time
 from datetime import datetime
-import sys
 
 
 # Defines how long to wait between captures
@@ -15,7 +14,7 @@ image_interval_ms = 1000
 capture_elapsed_ms = 0
 
 # Defines the parent folder to save images under
-save_folder = "D:\Run1"
+save_folder = "C:\\Users\\bowsert\\Downloads\\test"
 
 
 # Gets the current time in milliseconds, from epoch
@@ -40,14 +39,27 @@ if __name__ == "__main__":
     print(f'Beginning time lapse recording at an interval of {image_interval_ms/1000} seconds/photo')
     print('Hold [Ctrl]+[C], or [Esc] to exit')
     start_time_ms = get_current_ms()
-    stream_links = [1,2]
     
-    stream0 = cv2.VideoCapture(1)
-    stream1 = cv2.VideoCapture(2)
+    # For USB Camera
+    # stream0 = cv2.VideoCapture(1)
+
+    # For Intel Realsense Camera
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    pipeline.start(config)
+
     while True:
         if abs(start_time_ms - get_current_ms()) >= image_interval_ms:
-            ret, frame0 = stream0.read()
-            ret, frame1 = stream1.read()
-            cv2.imwrite(get_save_directory(0), frame0)
-            cv2.imwrite(get_save_directory(1), frame1)
+            # ret, frame0 = stream0.read()
+            # cv2.imwrite(get_save_directory(0), frame0)
+
+            frames = pipeline.wait_for_frames()
+            # depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            color_image = np.asanyarray(color_frame.get_data())
+            cv2.imwrite(get_save_directory(0), color_image)
+
+
             start_time_ms = get_current_ms()
